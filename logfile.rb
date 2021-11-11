96aba79ff25ce4dcde58814796e6f24cc9367d01
Jessica Levine <jessica.s.levine@gmail.com>
2021-11-11 16:23:25 -0500

    Add pretty printing and rbenv + bundler

--- logfile.rb
+++ logfile.rb
@@ -1,3 +1,7 @@
+require 'bundler'
+Bundler.require
+require 'colorize'
+
 class Logfile
   attr_accessor :path
   attr_accessor :parent_session
@@ -5,6 +9,12 @@ class Logfile
   def initialize(path, parent_session=nil)
     @path = path
     @parent_session = parent_session
+  end
+
+  def pp
+    session_str = "(#{parent_session.subfolder_id})".cyan.on_black
+    name = File.basename(path, File.extname(path)).capitalize.light_white.on_cyan
+    "#{session_str} #{name} \"#{path}\""
   end
 
   def to_s
