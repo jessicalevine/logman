58789e3db1f43359711f44327a3d7237438fa9c3
Jessica Levine <jessica.s.levine@gmail.com>
2021-11-11 17:06:57 -0500

    Improve logfile pretty printing

--- logfile.rb
+++ logfile.rb
@@ -12,9 +12,12 @@ class Logfile
   end
 
   def pp
-    session_str = "(#{parent_session.subfolder_id})".cyan.on_black
-    name = File.basename(path, File.extname(path)).capitalize.light_white.on_cyan
-    "#{session_str} #{name} \"#{path}\""
+    session_str = "(#{parent_session.subfolder_id})".ljust(5).cyan.on_black
+    name = File.basename(path, File.extname(path)).ljust(24).capitalize.light_white.on_cyan
+    ls = `ls -Clh \"#{path}\"`.split(" ")
+    date = "(Date: #{ls[5]} #{ls[6]} #{ls[7]})".ljust(20).cyan.on_black
+    size = "(Size: #{ls[4]})".ljust(13).light_white.on_cyan
+    "#{session_str} #{name} #{date} #{size} Path: \"#{path}\""
   end
 
   def to_s
